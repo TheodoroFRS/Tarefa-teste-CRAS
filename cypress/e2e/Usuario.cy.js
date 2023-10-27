bf6494a1ab9786a9daeef237663d43905921f7a5
Theodoro FRS <theodoroflordarosasatyro@gmail.com>
2023-10-27 16:47:39 -0400

    first commit

---
+++ cypress/e2e/Usuario.cy.js
@@ -0,0 +1,34 @@
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('https://front-cras.app.fslab.dev/');
+    cy.get("#email").type("diodete.moreira5030418844@gmail.com");
+    cy.get("#senha").type("123");
+    cy.get(" .styles_button__dr0t2").click();
+  })
+
+  it('Deve realizar realizar cadastro de um usuário com status ativo - cenário de sucesso', () => {
+
+    cy.get(".styles_buttonMenu__mmyUS > img").click();
+
+    cy.visit('https://front-cras.app.fslab.dev/usuarios/listar')
+    cy.get("    :nth-child(5) > .styles_button__dr0t2").click();
+
+    cy.get("#nomeCadastrar").type("Crasteste");
+    cy.get("#emailCadastrar").type("testeCras@gmail.com");
+    cy.get("#senhaCadastrar").type("asd");
+    
+    cy.get("#unidadeCadastrar").type("testeCras@gmail.com");
+    cy.get("#grupoCadastrar").type("testeCras@gmail.com");
+
+    cy.get('[type="submit"]').click();
+  })
+})
+
+
+// 2 Usuario.cy.js
+// Texto do caso de teste 'Deve realizar realizar cadastro de um usuário com status ativo - cenário de sucesso' 
+// Texto do caso de teste 'Deve retornar as mensagem de validação ao cadastrar usuário  - cenário de insucesso'
+// Texto do caso de teste 'Deve retornar uma consulta do usurário cadastrado com status ativo - cenário de sucesso'
+// Texto do caso de teste 'Deve retornar uma consulta do usurário cadastrado com status inativo - cenário de sucesso'
+// Texto do caso de teste 'Deve atualizar os dados de um usuário - cenário de sucesso ' 
+// Texto do caso de teste 'Deve visualizar as informações de um usuário - cenário de sucesso' 
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,50 @@
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('https://front-cras.app.fslab.dev/');
+  })
+
+  // it('Deve realizar o login com sucesso- cenário de  sucesso', () => {
+  //   cy.get("#email").type("diodete.moreira5030418844@gmail.com");
+  //   cy.get("#senha").type("123");
+  //   cy.get(" .styles_button__dr0t2").click();
+  // })
+
+  // it('Deve retornar mensagem de erro devido usuário inválido  - cenário de insucesso', () => {
+  //   cy.get("#email").type("diodete.moreira@gmail.com");
+  //   cy.get("#senha").type("123");
+  //   cy.get(" .styles_button__dr0t2").click();
+
+  //   cy.get('.Toastify__toast-body').contains('Usuário ou Senha inválida!'); 
+  // })
+
+  // it('Deve retornar msg dos campos obrigatório para login - cenário de insucesso', () => {
+  //   cy.get(" .styles_button__dr0t2").click();
+
+  //   cy.get(':nth-child(2) > .styles_formItem__H8I19 > .styles_errorMessage__IKSlh').contains('O email é obrigatório'); 
+  //   cy.get(':nth-child(3) > .styles_formItem__H8I19 > .styles_errorMessage__IKSlh').contains('A senha é obrigatória'); 
+
+  // })
+
+  //   it('Deve retornar mensagem de erro por senha ou usuário  - cenário de insucesso', () => {
+  //   cy.get("#email").type("diodete.moreira5030418844@gmail.com");
+  //   cy.get("#senha").type("12345678");
+  //   cy.get(" .styles_button__dr0t2").click();
+
+  //   cy.get('.Toastify__toast-body').contains('Usuário ou Senha inválida!'); 
+  // })
+
+
+})
+
+
+// 1 - login.cy.js - Deve  
+
+// Texto no describe 'Login'
+
+// Texto do caso de teste 'Deve realizar o login com sucesso- cenário de  sucesso' 
+
+// Texto do caso de teste 'Deve retornar mensagem de erro devido usuário inválido  - cenário de insucesso'
+
+// Texto do caso de teste 'Deve retornar msg dos campos obrigatório para login - cenário de insucesso'
+
+// Texto do caso de teste 'Deve retornar mensagem de erro por senha ou usuário  - cenário de insucesso'
